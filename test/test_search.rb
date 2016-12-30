f833c66dc9a0a4f1f065bd7b513549edcaff276a
ethan <yzhang.wa@gmail.com>
2016-12-30 14:49:48 +0800

    to initialize unit tests

---
+++ test/test_search.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'rails-simple-search'
+
+class RailsSimpleSearchTest < Minitest::Test
+  def test_one
+    assert_equal "hello world", "hello world"
+  end
+end
+
