f5244a721f2a922cc45d5af9a32c2e7f53a6e36f
Ethan Zhang <yzhang.wa@gmail.com>
2023-12-26 00:46:15 +0800

    refactored the code

--- lib/rails-simple-search.rb
+++ lib/rails-simple-search.rb
@@ -1,9 +1,10 @@
 require_relative 'sql_handler'
 
 module RailsSimpleSearch
-  DEFAULT_CONFIG = { :exact_match => [], 
-                     :or_separator => '_or_'
-                   }
+  DEFAULT_CONFIG = {
+    exact_match: [],
+    or_separator: '_or_'
+  }
 
   module FixModelName
     def model_name
@@ -16,16 +17,16 @@ module RailsSimpleSearch
 
     def self.inherited(subclass)
       class << subclass
-         # in rails 3, the call to "form_for" invokes the model_name 
-         def model_name
-           ActiveModel::Name.new(self)
-         end
+        # in rails 3, the call to "form_for" invokes the model_name
+        def model_name
+          ActiveModel::Name.new(self)
+        end
       end
-     
+
       # to fix an issues in rails 4.2
       subclass.send(:include, RailsSimpleSearch::FixModelName)
     end
- 
+
     def self.pre_process(model_name, &procedure)
       model_name.each { |internal_model_name| pre_process(internal_model_name, &procedure) } if model_name.is_a?(Array)
       @pre_processors ||= {}
@@ -37,7 +38,7 @@ module RailsSimpleSearch
       @pre_processors[model_name]
     end
 
-    def initialize(model_class, criteria={}, config={})
+    def initialize(model_class, criteria = {}, config = {})
       @criteria = sanitize_criteria(criteria)
       @config = DEFAULT_CONFIG.merge(config)
       @config[:exact_match] = [@config[:exact_match]] unless @config[:exact_match].respond_to?(:map!)
@@ -49,21 +50,19 @@ module RailsSimpleSearch
     end
 
     def load_database_handler(model_class)
-      if model_class.ancestors.include?(ActiveRecord::Base)
-        RailsSimpleSearch::Base.send(:include, RailsSimpleSearch::SqlHandler)
-      else
-        raise("RailsSimpleSearch only supports ActiveRecord for now")
-      end
+      raise("RailsSimpleSearch only supports ActiveRecord for now") unless model_class.ancestors.include?(ActiveRecord::Base)
+
+      RailsSimpleSearch::Base.send(:include, RailsSimpleSearch::SqlHandler)
     end
-  
+
     def count
       @count || 0
     end
 
-    def add_conditions(h={})
-      @criteria.merge!(h.stringify_keys)
+    def add_conditions(hash = {})
+      @criteria.merge!(hash.stringify_keys)
     end
-  
+
     def remove_criteria(key)
       key.each { |internal_key| remove_criteria(internal_key) } if key.is_a?(Array)
       @criteria.delete(key.to_s)
@@ -73,13 +72,13 @@ module RailsSimpleSearch
       @criteria[key.to_s] = value
     end
 
-    private 
+    private
 
     def method_missing(method, *args)
       method_str = method.to_s
       if method_str =~ /^([^=]+)=$/
-        @criteria[$1.to_s] = args[0]
-      else 
+        @criteria[::Regexp.last_match(1).to_s] = args[0]
+      else
         @criteria[method_str]
       end
     end
@@ -87,29 +86,19 @@ module RailsSimpleSearch
     def parse_field_name(name)
       result = {}
       if name =~ /^(.*)?((_(greater|less)_than)(_equal_to)?)$/
-        result[:field_name] = $1
-        if $4 == 'greater'
-          result[:operator] = ">"
-        else
-          result[:operator] = "<"
-        end
-        if $5
-          result[:operator] << "="
-        end
+        result[:field_name] = ::Regexp.last_match(1)
+        rresult[:operator] = (::Regexp.last_match(4) == 'greater' ? '>' : '<')
+        result[:operator] << '=' if ::Regexp.last_match(5)
       else
         result[:field_name] = name
       end
       result
     end
-  
+
     def sanitize_criteria(criteria)
-      criteria = criteria || {}
+      criteria ||= {}
       c = {}
-      criteria.each do |key, value|
-        unless value.blank?
-          c[key] = value
-        end
-      end
+      criteria.each { |key, value| c[key] = value unless value.blank? }
       c.stringify_keys
     end
   end
--- lib/sql_handler.rb
+++ lib/sql_handler.rb
@@ -1,6 +1,5 @@
 module RailsSimpleSearch
-  module SqlHandler  
-
+  module SqlHandler
     def init
       @table_name = @model_class.table_name
       @joins = {}
@@ -26,21 +25,21 @@ module RailsSimpleSearch
       elsif column.respond_to?(:type)
         column.type == :string || column.type == :text
       else
-        raise "encountered new version of Rails"
+        raise 'encountered new version of Rails'
       end
     end
-  
+
     def make_joins
       @joins_str = ''
       joins = @joins.values
-      joins.sort! {|a,b| a[0] <=> b[0]}
+      joins.sort! { |a, b| a[0] <=> b[0] }
       joins.each do |j|
         table = j[1]
         constrain = j[2]
-        @joins_str << " inner join  #{table} on #{constrain}" 
+        @joins_str << " inner join #{table} on #{constrain}"
       end
     end
-  
+
     def run_criteria
       @condition_group = ConditionGroup.new
       @condition_group.set_relation(:and)
@@ -59,7 +58,7 @@ module RailsSimpleSearch
 
       table = base_class.table_name
       key = "#{table}.#{field}"
-  
+
       column = base_class.columns_hash[field.to_s]
       return nil unless column
 
@@ -69,7 +68,7 @@ module RailsSimpleSearch
         verb = 'in'
       elsif operator
         verb = operator
-      elsif text_column?(column) && ! @config[:exact_match].include?((@table_name == table)? field : key)
+      elsif text_column?(column) && ! @config[:exact_match].include?((@table_name == table) ? field : key)
         verb = 'like'
         value = "%#{value}%"
       else
@@ -78,29 +77,28 @@ module RailsSimpleSearch
 
       ConditionGroup.new(ConditionItem.new(key, verb, value))
     end
-  
+
     def insert_join(base_class, asso_ref)
       base_table = base_class.table_name
       asso_table = asso_ref.klass.table_name
-      
+
       @join_count ||= 0
-      unless base_table == asso_table
-        if @joins[asso_table].nil?
-          @join_count += 1
-          if asso_ref.belongs_to?
-            @joins[asso_table] =[@join_count, asso_table, "#{base_table}.#{asso_ref.foreign_key} = #{asso_table}.#{asso_ref.klass.primary_key}"]
-          else
-            join_cond = "#{base_table}.#{base_class.primary_key} = #{asso_table}.#{asso_ref.foreign_key}"
-            join_cond = "#{asso_table}.#{asso_ref.type} = '#{base_class.name}' and #{join_cond}" if asso_ref.type
-            @joins[asso_table] = [@join_count, asso_table, join_cond]
-          end
-        end
+      return if base_table == asso_table
+      return unless @joins[asso_table].nil?
+
+      @join_count += 1
+      if asso_ref.belongs_to?
+        @joins[asso_table] =[@join_count, asso_table, "#{base_table}.#{asso_ref.foreign_key} = #{asso_table}.#{asso_ref.klass.primary_key}"]
+      else
+        join_cond = "#{base_table}.#{base_class.primary_key} = #{asso_table}.#{asso_ref.foreign_key}"
+        join_cond = "#{asso_table}.#{asso_ref.type} = '#{base_class.name}' and #{join_cond}" if asso_ref.type
+        @joins[asso_table] = [@join_count, asso_table, join_cond]
       end
     end
-  
+
     def parse_attribute(attribute, value)
       attributes = attribute.split(@config[:or_separator])
-      if(attributes.size > 1)
+      if attributes.size > 1
         cg = ConditionGroup.new
         cg.set_relation(:or)
         attributes.each do |a|
@@ -113,22 +111,20 @@ module RailsSimpleSearch
         field = attribute
         condition = insert_condition(@model_class, attribute, field, value)
         return condition
-      end 
+      end
 
       association_fields = attribute.split(/\./)
       field = association_fields.pop
 
       base_class = @model_class
-      while (association_fields.size > 0) 
+      until association_fields.empty?
         association_fields[0] = base_class.reflect_on_association(association_fields[0].to_sym)
         insert_join(base_class, association_fields[0])
         base_class = association_fields.shift.klass
       end
 
-      condition = insert_condition(base_class, attribute, field, value)
-      return condition
+      insert_condition(base_class, attribute, field, value)
     end
-  
   end
 
   class ConditionItem
@@ -142,7 +138,7 @@ module RailsSimpleSearch
   end
 
   class ConditionGroup
-    def initialize(item=nil)
+    def initialize(item = nil)
       if item
         @condition_item = item
       else
@@ -150,17 +146,15 @@ module RailsSimpleSearch
       end
     end
 
-    def add(cg)
-      if leaf?
-        raise "It's not allowed to add child into leaf node"
-      end
-      @children << cg if cg
+    def add(condition_group)
+      raise "It's not allowed to add child into leaf node" if leaf?
+
+      @children << condition_group if condition_group
     end
 
     def set_relation(and_or)
-      if leaf?
-        raise "It's not needed to set relation for leaf node"
-      end
+      raise "It's not needed to set relation for leaf node" if leaf?
+
       @relation = and_or
     end
 
@@ -169,7 +163,7 @@ module RailsSimpleSearch
     end
 
     def empty?
-      (@children && @children.empty?) ? true : false
+      @children && @children.empty? ? true : false
     end
 
     def to_ar_condition
@@ -177,17 +171,14 @@ module RailsSimpleSearch
       if leaf?
         i = @condition_item
         condition << "#{i.field} #{i.verb}"
-        if i.verb == 'in'
-          condition[0] << " (?)"
-        else
-          condition[0] << " ?"
-        end
+        condition[0] << (i.verb == 'in' ? ' (?)' : ' ?')
         condition << i.value
       else
-        tmp = @children.map(&:to_ar_condition)
-        condition << "(" + tmp.map(&:first).join(" #{@relation} ") + ")"
-        tmp.each do |t|
-          (1..(t.length-1)).each do |index|
+        tmp_conditions = @children.map(&:to_ar_condition)
+        tmp_condition_str = tmp_conditions.map(&:first).join(" #{@relation} ")
+        condition << "(#{tmp_condition_str})"
+        tmp_conditions.each do |t|
+          (1..(t.length - 1)).each do |index|
             condition << t[index]
           end
         end
